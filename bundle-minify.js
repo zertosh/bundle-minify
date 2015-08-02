292427626c97a4b71ba7bdce7f948d18a90f88ef
Andres Suarez <zertosh@gmail.com>
2015-08-02 19:03:55 -0400

    remove underscore and through2 deps

--- bundle-minify.js
+++ bundle-minify.js
@@ -1,20 +1,30 @@
-var _ = require('underscore');
-var through = require('through2');
+'use strict';
+
+var Transform = require('stream').Transform;
 var uglify = require('uglify-js');
 
 module.exports = function apply(b, opts_) {
-  var opts = _.extend({}, opts_, {fromString: true});
-  var buffers = [];
+  var opts = {fromString: true};
+  if (opts_) {
+    Object.keys(opts_).forEach(function(key) {
+      opts[key] = opts_[key];
+    });
+  }
 
-  b.pipeline.get('wrap').push(through(function(chunk, enc, next) {
+  var buffers = [];
+  var transformer = new Transform();
+  transformer._transform = function(chunk, enc, next) {
     buffers.push(chunk);
     next();
-  }, function(next) {
+  };
+  transformer._flush = function(next) {
     var source = Buffer.concat(buffers).toString();
     var minified = uglify.minify(source, opts);
     this.push(minified.code);
     next();
-  }));
+  };
+
+  b.pipeline.get('wrap').push(transformer);
 
   b.once('reset', function() {
     apply(b, opts_);
